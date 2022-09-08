31f69ae718ce5f54dd08af140ba72bbb3428c563
MrProstos <vladmihin28@gmail.com>
2022-09-08 17:42:55 +0300

    .

--- App/Controllers/Export.php
+++ App/Controllers/Export.php
@@ -33,7 +33,7 @@ class Export extends \Core\Controller
             header('Content-Type: application/vnd.ms-excel');
             header('Content-Disposition: attachment; filename="export.xls"');
 
-            $writer->save('php://output'); // TODO не сохранять , а сразу отправлять
+            $writer->save('php://output');
             exit();
         } catch (\Exception) {
             echo 'Ошибка экспорта';
@@ -41,29 +41,4 @@ class Export extends \Core\Controller
 
     }
 
-    /**
-     * Send file on user
-     * @param string $file
-     * @return void
-     */
-    private function sendFile(string $file): void
-    {
-        if (file_exists($file)) {
-
-            if (ob_get_level()) {
-                ob_end_clean();
-            }
-
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename=' . basename($file));
-            header('Content-Transfer-Encoding: binary');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($file));
-            readfile($file);
-            exit();
-        }
-    }
 }
